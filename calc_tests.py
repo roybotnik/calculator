194b2a008f167fd9a95fe43154d56890b87b5f61
Roy Quader <roy.quader@livingsocial.com>
2014-12-04 02:59:38 -0500

    Adding requirements.txt

--- calc_tests.py
+++ calc_tests.py
@@ -2,6 +2,7 @@
 import calc
 import unittest
 import tempfile
+from flask import jsonify
 
 class CalcTestCase(unittest.TestCase):
   def setUp(self):
@@ -14,6 +15,14 @@ class CalcTestCase(unittest.TestCase):
   def test_get_calculation_result_should_be_successful(self):
     response = self.app.get('/api/v1/calculation/result?expression=2%2B2')
     self.assertEquals(response.status_code, 200)
+
+  def test_get_calculation_result_should_return_result(self):
+    response = self.app.get('/api/v1/calculation/result?expression=2%2B2')
+    self.assertEquals(response.status_code, 200)
+
+  def test_get_calculation_result_should_404_if_no_expression_supplied(self):
+    response = self.app.get('/api/v1/calculation/result')
+    self.assertEquals(response.status_code, 404)
 
 if __name__ == '__main__':
   unittest.main()
